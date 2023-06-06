28dddcd4f038e333f8d3513e01be36dbbdaa2fea
3N3G <68242632+3N3G@users.noreply.github.com>
2023-06-06 15:39:28 -0700

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -127,7 +127,7 @@ def perim(r):
     return dist(p1(r),p2(r))+dist(p2(r),p3(r))+dist(p3(r),p4(r))+dist(p4(r),p1(r))
 
 def area(r):
-    return dist(p1(r),p2(r))*dist(p2(r),p3(r))
+    return dist(r[0],r[1])*dist(r[2],r[3])
 
 r1 = make_point(0,0)
 r2 = make_point(8,6)
@@ -159,8 +159,100 @@ Exercise 2.4
 def cons(x,y):
     return lambda m: m(x,y)
 def car(z):
-    return z(lambda x: x[0])
+    return z(lambda x,y: x)
 def cdr(z):
-    return z(lambda x: x[1])
+    return z(lambda x,y: y)
 
 print(car(cons(1,2)))
+
+"""
+Exercise 2.5
+"""
+
+def cons_ex5(x,y):
+    return 2**x*3**y
+
+def powerof(p, n):
+    if (n % p == 0):
+        return powerof(p, n/p)
+    else:
+        return 0
+def car_ex5(z):
+    return powerof(2,z)
+def cdr_ex5(z):
+    return powerof(3,z)
+
+
+"""
+Exercise 2.6
+"""
+
+zero = lambda f: lambda x: x
+
+def add_one(n):
+    return lambda f: lambda x: f(n(f)(x))
+
+#one = add_one(zero) = lambda f: lambda x: f(f(x))
+one = lambda f: lambda x: f(f(x))
+two = lambda f: lambda x: f(f(f(x)))
+
+def add(m,n):
+    return lambda f: lambda x: m(f)(n(f)(x))
+
+"""
+Exercise 2.7
+"""
+def make_interval(a,b):
+    return (a,b)
+
+def lower_bound(x):
+    return x[0]
+def upper_bound(x):
+    return x[1]
+
+"""
+Exercise 2.8
+"""
+
+def sub_interval(x,y): # interval that is x-y
+    return (lower_bound(x)-upper_bound(y), upper_bound(x)-lower_bound(y))
+
+
+"""
+Exercise 2.9
+
+Width of a sum only depends on the width of the summands:
+    width([a,b]+[c,d]) = width([a+c,b+d]) = ((b+d)-(a+c))/2 = width([a,b])+width([c,d])
+
+However, this is not true for multiplication because there is an "or" function:
+    width([a,b]*[c,d]) != width([a+10,b+10]*[c,d])
+Because division uses multiplication it must not be true for division either
+"""
+
+
+"""
+Exercise 2.10
+"""
+def div_interval: #assume alyssas program
+    return
+
+def divide_interval(x,y):
+    if (lower_bound(x)*upper_bound(x)<=0 or lower_bound(y)*upper_bound(y)>=0):
+        raise Exception("No intervals cross zero")
+    return div_interval(x,y)
+
+"""
+Exercise 2.11
+"""
+
+
+
+
+
+
+
+
+
+
+
+
